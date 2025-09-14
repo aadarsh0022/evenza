ca9bfd0a44005565c48e07ed1c86c28fa51e78f4
Aadarsh <aadarsh0022@gmail.com>
2025-09-14 21:54:41 +0530

    Initial Setup project

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <main>
+      <h1 className='text-3xl'>Evenza</h1>
+    </main>
+  )
+}
+
+export default page
---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "Evently is a event management platform",
+  icons: {
+    icon: "/assets/images/logo.svg",
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={poppins.variable}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
